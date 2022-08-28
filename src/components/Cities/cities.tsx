f7786357f655818bce980508e34459e4f08649ef
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-27 23:36:50 -0300

    feat: Adding city page and other tweaks

--- src/components/Cities/cities.tsx
+++ src/components/Cities/cities.tsx
@@ -1,12 +1,22 @@
 import style from '@/styles/components/cities.module.scss'
+import { useRouter } from 'next/router'
 import { cities } from '../../mocks/'
 
 export const Cities = () => {
+  const router = useRouter()
+
+  const handleClick = (city: string, country: string) => {
+    router.push({
+      pathname: '/[city]',
+      query: { city: `${city},${country}` },
+    })
+  }
+
   return (
     <div className={style.citiesContent}>
-      {cities.map((city) => (
-        <a href='' key={city.id.toString()}>
-          {city.name}
+      {cities.map(({ id, city, country }) => (
+        <a onClick={() => handleClick(city, country)} key={id.toString()}>
+          {city}
         </a>
       ))}
     </div>
