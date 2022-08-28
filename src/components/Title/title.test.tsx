0bae2bae227316cf8e68eed17ccdfcf630ccbeb2
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-27 22:34:12 -0300

    test: adjusting tests

--- src/components/Title/title.test.tsx
+++ src/components/Title/title.test.tsx
@@ -3,13 +3,12 @@ import { Title } from '.'
 
 describe('<Title />', () => {
   it.each(['title'])('should have a %p element', (expected) => {
-    const { queryAllByText } = getRenderer()
-
-    const titles = queryAllByText(expected)
-
-    titles.map((title) => {
-      expect(title).toBeInTheDocument()
-    })
+    const { getByRole } = getRenderer()
+    expect(getByRole('heading', { name: expected })).toBeInTheDocument()
+  })
+  it.each(['subtitle'])('should have a %p element', () => {
+    const { getByText } = getRenderer()
+    expect(getByText('select a city')).toBeInTheDocument()
   })
 })
 
