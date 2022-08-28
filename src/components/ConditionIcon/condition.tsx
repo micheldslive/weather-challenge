dbc07673417ce1af09fe4303584ae9514c7d72a4
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-28 20:57:15 -0300

    feat: creating conditionIcon component

---
+++ src/components/ConditionIcon/condition.tsx
@@ -0,0 +1,9 @@
+import Image from '../../mocks/images'
+
+export const ConditionIcon = () => {
+  return (
+    <div className='conditionContent'>
+      <p>{Image['all']['world']}</p>
+    </div>
+  )
+}
