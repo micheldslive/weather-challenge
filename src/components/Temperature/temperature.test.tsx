77017c6416f4fa543e510bf7c393b0fe50559a05
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-28 18:24:39 -0300

    refactor: general change in the way of importing sass

--- src/components/Temperature/temperature.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import { Temperature } from '.'
-
-describe('<Temperature />', () => {
-  it.each(['temperature title'])('should have a %p element', () => {
-    const { getByRole } = render(<Temperature />)
-    expect(getByRole('heading', { level: 1 })).toBeInTheDocument()
-  })
-})
--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -1,13 +1,11 @@
-import style from '@/styles/pages/home.module.scss'
-
 import { Title } from '@/components/Title/'
 import { World } from '@/components/World'
 import { Cities } from '@/components/Cities'
 
 export const Home = () => {
   return (
-    <div className={style.content}>
-      <div className={style.container}>
+    <div className='homeContent'>
+      <div className='homeContainer'>
         <Title />
         <World />
         <Cities />
