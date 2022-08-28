2d5ac9c6eada882322e6b65cbc660fe376ed91fc
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-28 18:23:49 -0300

    chore: changing maxmin for the temperatures component

--- src/components/MaxMin/maxmin.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import { MaxMin } from '.'
-
-describe('<Temperature />', () => {
-  it.each(['temperature title'])('should have a %p element', () => {
-    const { getByLabelText } = render(<MaxMin />)
-    expect(getByLabelText('maxmin')).toBeInTheDocument()
-  })
-})
