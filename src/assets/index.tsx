539c0fd71daebe729d990559c1735b875f8a09a5
Michel Domingos <michel.silva@penseavanti.com.br>
2022-08-28 17:44:47 -0300

    asset: adding images to the project

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,7 +1,15 @@
 import WorldIcon from './world-icon.svg'
+import BackIcon from './back-button.svg'
+import MaxMinIcon from './max-min-arrows.svg'
 
-const Image = {
+type TImage = {
+  [key: string]: JSX.Element
+}
+
+const Image: TImage = {
   world: <WorldIcon />,
+  back: <BackIcon />,
+  maxmin: <MaxMinIcon />,
 }
 
 export default Image
